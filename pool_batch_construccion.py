7defc53941639c4a59042110cc67da285e155a40
jarobyte <caballazo.loco@gmail.com>
2018-06-22 18:54:02 -0500

    argumentos de sistema en pool_batch_construccion

--- pool_batch_construccion.py
+++ pool_batch_construccion.py
@@ -4,6 +4,7 @@ from multiprocessing import Pool
 import construccion as c
 from tqdm import tqdm
 from deben_fusionarse.definiciones import existen_identificadores as function
+import argparse
 
 def fun(tupla):
     if function(registros[tupla[0]],registros[tupla[1]]):
@@ -13,8 +14,14 @@ def fun(tupla):
 def fun_filt(batch):
     return list(filter(fun, batch))
 
-registros = pd.read_csv("import/datos/registros.csv").values
-parejas = itertools.combinations(range(registros.shape[0]), 2)
+parser = argparse.ArgumentParser(description = "")
+parser.add_argument('lower_limit', type = int)
+parser.add_argument('upper_limit', type = int)
+args = parser.parse_args()
+lower_limit = int(args.lower_limit)
+upper_limit = int(args.upper_limit)
+registros = pd.read_csv("import/datos/prueba_registros.csv").values
+parejas = itertools.combinations(range(lower_limit, upper_limit), 2)
 numero_de_parejas = int((registros.shape[0]) * (registros.shape[0] - 1) / 2)
 chunk_size = 10000
 p = Pool()
@@ -25,4 +32,4 @@ resultado = [x for x in tqdm(p.imap_unordered(func = fun,
 
 with open("parejas.txt", 'w+') as archivo:
     archivo.write(str(resultado))
-    #archivo.write("\n")
+    archivo.write("\n")
