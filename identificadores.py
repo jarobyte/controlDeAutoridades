6ed26aaf365c3922517d422022017f5c62b6e7da
Juan Ramirez <caballazo.loco@gmail.com>
2018-07-05 01:47:26 -0500

    FINALgit status!

---
+++ identificadores.py
@@ -0,0 +1,103 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import multiprocessing as mp
+
+
+# In[2]:
+
+
+with open("personas.txt", "r") as archivo:
+    cadena = archivo.read()
+lista = eval(cadena)
+personas = list(enumerate(lista))
+
+
+# In[3]:
+
+
+base = pd.read_csv("import/datos/registros.csv")
+df = base.values
+
+
+# In[4]:
+
+
+def enviar_a_identificadores(tupla):
+    persona = df[tupla[1]]
+    identificador_CA = tupla[0]
+    curps_persona = persona[:, [0,13]]
+    cvus_persona = persona[:,[7,13]]
+    orcids_persona = persona[:,[8,13]]
+    rns_persona = persona[:,[9,13]]
+    dni_persona = persona[:,[10,13]]
+    identificadores = ([[identificador_CA] + ["curp"] + list(renglon) for renglon in curps_persona] + 
+                   [[identificador_CA] + ["cvu"] + list(renglon) for renglon in cvus_persona] + 
+                   [[identificador_CA] + ["orcid"] + list(renglon) for renglon in orcids_persona] + 
+                   [[identificador_CA] + ["rn"] + list(renglon) for renglon in rns_persona])
+    return identificadores
+
+
+# In[12]:
+
+
+enviar_a_identificadores(personas[2])
+
+
+# In[13]:
+
+
+personas[2]
+
+
+# In[6]:
+
+
+tupla = personas[0]
+persona = df[tupla[1]]
+identificador_CA = tupla[0]
+curps_persona = persona[:, [0,13]]
+cvus_persona = persona[:,[7,13]]
+orcids_persona = persona[:,[8,13]]
+rns_persona = persona[:,[9,13]]
+dni_persona = persona[:,[10,13]]
+[[identificador_CA] + list(renglon) for renglon in curps_persona]
+identificadores = ([[identificador_CA] + ["curp"] + list(renglon) for renglon in curps_persona] + 
+                   [[identificador_CA] + ["cvu"] + list(renglon) for renglon in cvus_persona] + 
+                   [[identificador_CA] + ["orcid"] + list(renglon) for renglon in orcids_persona] + 
+                   [[identificador_CA] + ["rn"] + list(renglon) for renglon in rns_persona])
+identificadores
+
+
+# In[7]:
+
+
+per = personas[0:10]
+final = []
+p = mp.Pool()
+for i in list(p.map(enviar_a_identificadores, per)):
+    final += i
+final
+
+
+# In[10]:
+
+
+identificadores = pd.DataFrame(final, columns = ["identificador_CA", "tipo_de_identificador", "valor", "origen"])
+
+
+# In[11]:
+
+
+identificadores
+
+
+# In[14]:
+
+
+identificadores.to_csv("identificadores.csv")
+
