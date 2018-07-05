f88695875569b0c5e6b4bae8a38c764e9ff154cb
Juan Ramirez <caballazo.loco@gmail.com>
2018-07-05 01:07:43 -0500

    variantes_de_nombres.py

---
+++ variantes_de_nombres.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import multiprocessing as mp
+
+with open("personas.txt", "r") as archivo:
+    cadena = archivo.read()
+lista = eval(cadena)
+personas = list(enumerate(lista))
+
+base = pd.read_csv("import/datos/registros.csv")
+df = base.values
+
+def enviar_a_variantes_de_nombres(tupla):
+    persona = df[tupla[1]]
+    
+    identificador_CA = tupla[0]
+    nombres = persona[:,[3,1,2,13]]
+    return [[identificador_CA] + list(renglon) for renglon in nombres]
+
+per = personas[0:10]
+final = []
+p = mp.Pool()
+for i in list(p.map(enviar_a_variantes_de_nombres, per)):
+    final += i
+variantes_de_nombres = pd.DataFrame(final, columns = ["identificador_CA", 
+                                                      "variante_de_nombre", 
+                                                      "variante_de_primer_apellido",
+                                                      "variante_de_segundo_apellido",
+                                                      "origen"])
+variantes_de_nombres.to_csv("variantes_de_nombres.csv")
+
